861eb71866be13c33539d6598302973c0bd2f377
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-09-04 06:29:52 +0200

    Hero section completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,43 @@
 import { Button } from '@/components/ui/button';
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>    
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Discover and Share Unforgettable Moments!</h1>
+            <p className="p-regular-20 md:p-regular-24">
+            Join our vibrant community of event enthusiasts! Whether you're hosting or 
+            attending, our platform makes it easy to explore, organize, and enjoy events that matter
+             to you. Start creating memories today.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Events
+              </Link>
+            </Button>
+          </div>
+
+          <Image 
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+
+      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+        <h2 className='h2-bold'>
+        Trusted by <br /> Thousands of Events!
+        </h2>
+
+
+      </section>
+    </>
   );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+// import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link className='text-primary-500' href='/'>
+          Sthiba Events..
+        </Link>
+        <Link
+          href='https://lebogang-developer.netlify.app/'
+          target='_blank'
+        >
+          Lebo Sekaleli
+        </Link>
+        <p>2024 Sthiba Events. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
-Footer
+export default Footer;
